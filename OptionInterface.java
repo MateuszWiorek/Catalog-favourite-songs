89d3154d63245f31798a84d2ee9a9d293ebac4f0
MateuszWiorek <mateusz.wiorek@onet.pl>
2018-11-27 12:09:07 +0100

    Refactor structure of project

--- OptionInterface.java
+++ OptionInterface.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import static com.wiorekmateusz.FavouriteSongsInterface.scrollPane;
 
 public class OptionInterface extends JPanel implements ActionListener{
     private JTextField author, title;
